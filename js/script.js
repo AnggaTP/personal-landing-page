9a75e62b36e22a8ce51cefa98cf01e6f51597fb7
AnggaTP <anggapriantoko2000@gmail.com>
2023-07-12 10:46:28 +0700

    fill content in project section

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('.profile-button')
+const floatbtn = document.querySelector('.floating-button')
+
+btn.addEventListener('click',()=>{
+    location.href = 'https://github.com/AnggaTP';
+})
+
+floatbtn.addEventListener('click',()=>{
+    location.href = '.header';
+})
